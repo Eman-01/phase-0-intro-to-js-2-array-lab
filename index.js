faaa6999ebe4d627d03b2c703ecbf91104b35c27
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2024-04-04 15:14:13 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield')
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo')
+}
+function appendCat(name){
+    const cats2 = cats.slice()
+    cats2.push(name)
+    return cats2;
+}
+function prependCat(name){
+    const cats3 = cats.slice()
+    cats3.unshift(name)
+    return cats3;
+}
+function removeLastCat(name){
+    const cats4 = cats.slice()
+    cats4.pop(name)
+    return cats4;
+}
+function removeFirstCat(name){
+    const cats5 = cats.slice()
+    cats5.shift(name)
+    return cats5;
+}
